7df05f94d7e9e4945f82f557136fbe3c7eca8e1c
Kaibo Ma <you@example.com>
2017-01-25 22:08:58 -0800

    added work and projects

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+
+
 var previousScroll = 0,
     headerOrgOffset = $('#home-top').height();
 
@@ -15,16 +17,54 @@ $(window).scroll(function () {
     previousScroll = currentScroll;
 });
 
-$('#about').click(function() {
+$(function() {
+  $('#main').smoothState();
+});
 
-    $(this).animate({
-        left: '-50%'
-    }, 500, function() {
-        $(this).css('left', '150%');
-        $(this).appendTo('#container');
+(function ($) {
+
+    'use strict';
+
+    $(document).ready(function () {
+
+        // Init here.
+        var $body = $('body'),
+            $main = $('#main'),
+            $site = $('html, body'),
+            transition = 'fade',
+            smoothState;
+
+        smoothState = $main.smoothState({
+            onBefore: function($anchor, $container) {
+                var current = $('[data-viewport]').first().data('viewport'),
+                    target = $anchor.data('target');
+                current = current ? current : 0;
+                target = target ? target : 0;
+                if (current === target) {
+                    transition = 'fade';
+                } else if (current < target) {
+                    transition = 'moveright';
+                } else {
+                    transition = 'moveleft';
+                }
+            },
+            onStart: {
+                duration: 400,
+                render: function (url, $container) {
+                    $main.attr('data-transition', transition);
+                    $main.addClass('is-exiting');
+                    $site.animate({scrollTop: 0});
+                }
+            },
+            onReady: {
+                duration: 0,
+                render: function ($container, $newContent) {
+                    $container.html($newContent);
+                    $container.removeClass('is-exiting');
+                }
+            },
+        }).data('smoothState');
+
     });
 
-    $(this).next().animate({
-        left: '50%'
-    }, 500);
-});
+}(jQuery));
